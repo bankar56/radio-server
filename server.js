df0ba853b4f9af6bf6d6838447a11261fec1b99c
bankar56 <bankarevo28@gmail.com>
2026-01-12 20:41:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const wss = new WebSocket.Server({ port: PORT });
 
 wss.on("connection", ws => {
   ws.on("message", msg => {
-    // просто рассылаем всем кроме отправителя
+    // просто ретранслируем signaling
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(msg);
