99d9a5a9c6ed8ff5509200e11eee52e60901e2f9
bankar56 <bankarevo28@gmail.com>
2026-01-12 20:30:27 +0200

    server.js

--- server.js
+++ server.js
@@ -3,32 +3,15 @@ const WebSocket = require("ws");
 const PORT = process.env.PORT || 3000;
 const wss = new WebSocket.Server({ port: PORT });
 
-let speaker = null;
-
 wss.on("connection", ws => {
   ws.on("message", msg => {
-    const data = JSON.parse(msg);
-
-    if (data.type === "start" && !speaker) {
-      speaker = ws;
-    }
-
-    if (data.type === "stop" && ws === speaker) {
-      speaker = null;
-    }
-
-    if (data.type === "audio" && ws === speaker) {
-      wss.clients.forEach(client => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(msg);
-        }
-      });
-    }
-  });
-
-  ws.on("close", () => {
-    if (ws === speaker) speaker = null;
+    // просто рассылаем всем кроме отправителя
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
   });
 });
 
-console.log("Radio server running on port", PORT);
+console.log("WebRTC signaling server on", PORT);
